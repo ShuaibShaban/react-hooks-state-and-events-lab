c028db845c322e7feb6bf7ac63bb991f8c25f3d8
shuaib shaban <shuaib>
2023-01-14 08:53:43 +0300

    items.jsupdate

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
 function Item({ name, category }) {
-  const [inCart, setListClass] = useState(false);
-  function handleAddCart() {
-    setListClass(!inCart);
+  const [itemInCart, setListClass] = useState(false);
+  function handleAddToCart() {
+    setListClass(!itemInCart);
   }
   return (
-    <li className={inCart ? "in-cart" : ""}>
+    <li className={itemInCart ? "itemIn-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className={inCart ? "remove" : "add"} onclick={handleAddCart}>
-        {inCart ? "Remove from Cart" : "Add to Cart"}
+      <button className={itemInCart ? "remove" : "add"} onclick={handleAddToCart}>
+        {itemInCart ? "Remove from Cart" : "Add to Cart"}
       </button>
     </li>
   );
